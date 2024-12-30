1c7fadbe7fa41b136dc900d7aa72f72910da60e2
Ayushi Singh <ayushi.23b0231017@abes.ac.in>
2024-12-30 16:02:53 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -3,4 +3,4 @@ sal = st.number_input('enter your salary')
 credit = st.number_input('enter your credit score')
 if sal >= 50000 and credit >= 500:
       st.write("CONGRALUTATIONS ")
-      st.hearts()
+      st.balloons()
